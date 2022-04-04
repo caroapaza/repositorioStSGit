937b42d214c224760e28c442715598e6672e8216
caroapaza <apazacarolina@gmail.com>
2022-04-04 19:41:59 -0300

    primer commit

---
+++ ProjectDemoGit/src/main/java/ar/edu/unju/fi/projectdemo/ProjectDemoGitApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.projectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDemoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDemoGitApplication.class, args);
+	}
+
+}
---
+++ ProjectDemoGit/src/test/java/ar/edu/unju/fi/projectdemo/ProjectDemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.projectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
