feff0b8121eda8eebe059ac14d7376a47d990807
anita <anita@anita>
2022-04-11 01:46:25 -0300

    Agrego getter's and setter's a entida Alumno

--- ProjectDemoGit/src/main/java/ar/edu/unju/fi/projectdemo/model/Alumno.java
+++ ProjectDemoGit/src/main/java/ar/edu/unju/fi/projectdemo/model/Alumno.java
@@ -3,4 +3,17 @@ package ar.edu.unju.fi.projectdemo.model;
 public class Alumno {
 	private String nombre;
 	private Integer documento;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getDocumento() {
+		return documento;
+	}
+	public void setDocumento(Integer documento) {
+		this.documento = documento;
+	}
+	
 }
